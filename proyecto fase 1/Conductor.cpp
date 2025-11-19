1a7df23bde98526b1c97082b7a924bce493638ec
OscarMonterrosa50 <monterrosaoscar50@gmail.com>
2025-11-18 22:26:57 -0600

    Delete proyecto fase 1 directory

--- proyecto fase 1/Conductor.cpp
+++
@@ -1,9 +0,0 @@
-#include "Conductor.h"
-Conductor::Conductor(string n, string a, int id, int seg, int tel)
-    : nombre(n), apellido(a), documentoID(id), numeroSeguro(seg), telefono(tel) {}
-
-string Conductor::getNombre() const { return nombre; }
-string Conductor::getApellido() const { return apellido; }
-int Conductor::getDocumentoID() const { return documentoID; }
-int Conductor::getNumeroSeguro() const { return numeroSeguro; }
-int Conductor::getTelefono() const { return telefono; }
--- proyecto fase 1/Funciones.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
-#include <string>
-using namespace std;
-
-int leerEntero(string mensaje);
-string leerTexto(string mensaje);
-
-#endif
